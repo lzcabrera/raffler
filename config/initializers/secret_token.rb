ef6f36c7605d2e1ee92be1ee204cdcd64322015f
Laura Cabrera <lzcabrera@gmail.com>
2012-04-10 19:15:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'fa02ab87e98a3a6becd7e33f951cf78e88622c170191ca8644b5960836e421ea56f9c0018016cc82c8ef95c215e552f0e27889985413176b419d51fc37fd020a'
